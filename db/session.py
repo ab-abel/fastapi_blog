89b25e57acde326c13356346b16847bd19cd703b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-04-07 13:21:24 +0100

    added session for db connection

---
+++ db/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+from typing import Generator
+
+SQL_DB_URL =settings.POSTGRES_URL
+
+engine = create_engine(SQL_DB_URL)
+
+
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#depenencdy injection
+
+def get_db() -> Generator:
+    try:
+        db=sessionLocal()
+        yield db
+    finally:
+        db.close()
