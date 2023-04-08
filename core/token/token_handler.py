aa7b28f4659eb99e393da2c85a1f525952b3b531
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-04-08 18:02:20 +0100

    token

--- core/token/token_handler.py
+++ core/token/token_handler.py
@@ -3,14 +3,15 @@ import datetime
 
 from fastapi import HTTPException, status
 from jose import jwt, JWTError
+
 from core.config import Settings
 
 settings = Settings()
 
 def create_access_token(user:str)->str:
-    payload ={
+    payload = {
         "user": user,
-        "expires":time.time()+ 3600
+        "expires":time.time()+ 3600,
     }
 
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
@@ -33,3 +34,8 @@ def verify_access_token(toke:str)->dict:
                 detail="Token expired!"
             )
         return data
+
+    except JWTError:
+        raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid token")
