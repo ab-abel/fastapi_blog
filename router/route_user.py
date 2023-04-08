d70f00ec8340c75585c21bfc64f7b19886283806
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-04-09 00:39:46 +0100

    added OAuth form for login

--- router/route_user.py
+++ router/route_user.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, status,Depends
 from db.model.users import RegisterUser, LoginUser, Profile
 
-from db.controller.users import registerUser
-from db.controller.users import loginUser
+from db.controller.users import registerUser, loginUser
+
+from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy.orm import Session
 from db.session import get_db
@@ -23,11 +24,11 @@ def create(user: RegisterUser, db:Session =Depends(get_db)):
     return user
 
 @router.post('/login')
-def login(user: LoginUser, db:Session=Depends(get_db)):
+def login(user: OAuth2PasswordRequestForm = Depends(OAuth2PasswordRequestForm), db:Session=Depends(get_db)):
     # print(user.email)
+    #to use OAuth change email to username
     user_login = loginUser.login(user, db)
     return user_login
-
 
 @router.put('/{id}')
 async def edit(id):
